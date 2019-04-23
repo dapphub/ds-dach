21db8bdb6b111281ddf75011ff87842bc2feffdd
Martin Lundfall <martin.lundfall@protonmail.com>
2019-04-24 00:58:45 +0200

    Updated to comply with the hectic modernity of our times

--- README.md
+++ README.md
@@ -1,48 +1,21 @@
 # ds-dach
-**Dappsys/Dai Automatic Clearing House** is a module that can be plugged in to a token contract to allow users to pay for simple token transfers _with the token being transacted_ rather than paying for gas costs using ether.
+The clearing house can clear cheques, allowing users to transfer tokens without eth.
 
 A `Cheque` is a signed message containing the following information:
 
 * `sender : address`, the author of the cheque
 * `receiver : address`, the receiver of the funds
 * `amount : uint256`, a token amount to transfer to the receiver from the `sender`
+* `expiry : uint256`, a time after which the cheque can no longer be cleared
 * `fee : uint256` a token amount to be transfered from the `sender` to whoever submits the cheque to the clearing house
-* `nonce: uint256`, for replay protection.
+* `nonce: uint256`, for replay protection
 
-Such cheques can be submitted by anyone to the clearing house through the following clearing function:
-```
-  function clear(address _sender, address _receiver, uint _amount, uint _fee, uint _nonce, uint8 v, bytes32 r, bytes32 s) public {
-    Cheque memory cheque = Cheque({
-      sender   : _sender,
-      receiver : _receiver,
-      amount   : _amount,
-      fee      : _fee,
-      nonce    : _nonce
-    });
-    require(verify(cheque, v, r, s));
-    require(cheque.nonce == nonces[cheque.src]);
-    mover.move(cheque.sender, msg.sender, cheque.fee);
-    mover.move(cheque.sender, cheque.receiver, cheque.amount);
-    nonces[cheque.sender]++;
-  }
-```
-where `move` is a function which has the authority to update token balances:
-```
-  function move(address src, address dst, uint wad) public {
-    balances[src] = sub(balances[src], wad);
-    balances[dst] = add(balances[dst], wad);
-  }
-
-```
+Anyone who obtains such a signed message can clear them, paying the gas cost of the transaction in exchange for the fee.
 
 ## Signature generation
-The contract uses the typed signature format of [EIP-712](https://github.com/ethereum/EIPs/blob/master/EIPS/eip-712.md) to provide a nice user interface and separate similar looking `Cheques` that might be used by other clearing houses.
-
-## Missing features
-- The EIP712 domain separator field should take the address of the verifying contract as input, but right now it is hard coded as 0xdeadbeef.
-- Possible gas optimization
+The contract uses the typed signature format of [EIP-712](https://github.com/ethereum/EIPs/blob/master/EIPS/eip-712.md) to provide a nice user interface and separate similar looking `Cheques` that might be used by other clearing houses. You can generate cheques with the accompanying `seth` script [here](/bin/cheque).
 
 ## Miner frontrunning
 Since cheques can be cleared by anyone, giving the fee to `msg.sender`, if a miner sees a transaction to clear a particular check, they can extract the cheque and clear it themselves, claiming the `fee` for themselves. 
 
-Although this provides an unfortunate situation for people aiming to clear cheques, it does not harm the author of the cheque. Therefore, we do not consider this issue to be of particular concern. One could imagine designing another type of Cheque that explcitly specifies who may clear it, but this is not done in this contract.
+Although this provides an unfortunate situation for people aiming to clear cheques, it does not harm the signer of the cheque. Therefore, we do not consider this issue to be of particular concern. One could imagine designing another type of Cheque that explcitly specifies who may clear it, but this is not done in this contract.
--- src/dach.sol
+++ src/dach.sol
@@ -1,31 +1,51 @@
+/// dach.sol -- An automated clearing house
+
+// Copyright (C) 2019  Martin Lundfall
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 pragma solidity >=0.4.23;
-import "./dai.sol";
 
-// Solidity Interface
+contract Uniswappy {
+    function tokenToEthTransferInput(uint256 tokens_sold, uint256 min_tokens,
+                                     uint256 deadline, address recipient) external returns (uint256) {}
+}
 
-contract Uniswap {
-    function tokenToEthTransferInput(uint256 tokens_sold, uint256 min_tokens, uint256 deadline, address recipient) external returns (uint256  eth_bought);
+contract DaiLike {
+  function transferFrom(address src, address dst, uint wad) public returns (bool) {}
+  function approve(address usr, uint wad) public returns (bool) {}
 }
 
 
 contract Dach {
-  Dai dai;
-  Uniswap uniswap;
+  DaiLike dai;
+  Uniswappy uniswap;
   mapping (address => uint256) public nonces;
 
   // --- EIP712 niceties ---
   bytes32 public DOMAIN_SEPARATOR;
   bytes32 constant public CHEQUE_TYPEHASH = keccak256(
-     "Cheque(address sender,address receiver,uint256 amount,uint256 fee,uint256 nonce, uint256 deadline)"
+     "Cheque(address sender,address receiver,uint256 amount,uint256 fee,uint256 nonce,uint256 expiry)"
   );
 
   bytes32 constant public SWAP_TYPEHASH = keccak256(
-     "Swap(address sender,uint256 amount,uint256 min_eth,uint256 fee,uint256 nonce,uint256 deadline)"
+     "Swap(address sender,uint256 amount,uint256 min_eth,uint256 fee,uint256 nonce,uint256 expiry)"
   );
 
-  constructor(Dai _dai, Uniswap _uniswap, string memory version, uint256 chainId) public {
-    dai = _dai;
-    uniswap = _uniswap;
+  constructor(address _dai, address _uniswap, string memory version, uint256 chainId) public {
+    dai = DaiLike(_dai);
+    uniswap = Uniswappy(_uniswap);
     DOMAIN_SEPARATOR = keccak256(abi.encode(
             keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
             keccak256("Dai Automated Clearing House"),
@@ -35,10 +55,9 @@ contract Dach {
         ));
   }
 
-  
-  function clear(address sender, address receiver, uint amount, uint fee,
-                 uint nonce, uint deadline, uint8 v, bytes32 r, bytes32 s) public
-  {
+
+  function clear(address sender, address receiver, uint amount, uint fee, uint nonce,
+                 uint expiry, uint8 v, bytes32 r, bytes32 s, address taxMan) public {
     bytes32 digest =
       keccak256(abi.encodePacked(
          "\x19\x01",
@@ -49,17 +68,18 @@ contract Dach {
                               amount,
                               fee,
                               nonce,
-                              deadline))
+                              expiry))
       ));
     require(sender == ecrecover(digest, v, r, s), "invalid cheque");
-    require(nonce == nonces[sender]++);
-    dai.transferFrom(sender, msg.sender, fee);
+    require(nonce == nonces[sender]++, "invalid nonce");
+    require(expiry == 0 || now <= expiry, "cheque expired");
+    dai.transferFrom(sender, taxMan, fee);
     dai.transferFrom(sender, receiver, amount);
   }
 
-  function swapToEth(address sender, uint amount, uint min_eth, uint fee,
-                     uint nonce, uint deadline, uint8 v, bytes32 r, bytes32 s) public {
-    bytes32 digest =
+  function swapToEth(address payable sender, uint amount, uint min_eth, uint fee, uint nonce,
+                     uint expiry, uint8 v, bytes32 r, bytes32 s, address taxMan) public returns (uint256) {
+    require(sender == ecrecover(
       keccak256(abi.encodePacked(
          "\x19\x01",
          DOMAIN_SEPARATOR,
@@ -69,12 +89,12 @@ contract Dach {
                               min_eth,
                               fee,
                               nonce,
-                              deadline))
-      ));
-    require(sender == ecrecover(digest, v, r, s), "invalid swap");
-    require(nonce == nonces[sender]++);    
+                              expiry)))), v, r, s), "invalid swap");
+    require(nonce == nonces[sender]++, "invalid nonce");
+    require(expiry == 0 || now <= expiry, "swap expired");
     dai.transferFrom(sender, address(this), amount);
-    uniswap.tokenToEthTransferInput(amount, min_eth, deadline, sender);
-    dai.transferFrom(sender, msg.sender, fee);
+    dai.transferFrom(sender, taxMan, fee);
+    dai.approve(address(uniswap), amount);
+    return uniswap.tokenToEthTransferInput(amount, min_eth, now, sender);
   }
 }
